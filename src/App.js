459715741a000c06218964295b3d53c7baf51c4a
abdulhamid469 <abdulhamid469@users.noreply.github.com>
2020-06-07 14:59:48 +0500

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+//import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Hello World </h1>
+      <h2> Welcome To ReactJS </h2>
+      <h3> Good Morning </h3>
+      <h4> Good Bye </h4>
+    </div>
+  );
+}
+
+export default App;
